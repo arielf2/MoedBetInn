9e05a100e068348fa2232aab2679d9f40bae3af0
rotemhec <rotemhecht@mail.tau.ac.il>
2019-12-19 19:49:18 +0200

    added get names from file

--- MoedBetInn/Header.h
+++ MoedBetInn/Header.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define MAX_NUMBER_OF_GUESTS 15
-#define ROOMS_FILE_NAME 13
+#define ROOMS_NAMES_FILE_NAME 13
 #define ROOM_GUEST_NAME_LEN 21
 #define MAX_LINE_LEN 51
 #define MAX_ROOMS 5
@@ -12,6 +12,12 @@ typedef struct _room
 	int price_for_night;
 	int max_guests;
 } room;
+
+typedef struct _guest
+{
+	char name[ROOM_GUEST_NAME_LEN];
+	int nigths;
+} guest;
 
 
 void CreateRoom_UpdateArray(char str[], room *room_array[], int index);
--- MoedBetInn/main.c
+++ MoedBetInn/main.c
@@ -21,39 +21,72 @@ int main(int argc, char *argv[]) {
 	HANDLE countMutex = CreateMutex(NULL, FALSE, "countMutex");
 	HANDLE logFileMutex = CreateMutex(NULL, FALSE, "logFileMutex");
 	HANDLE barrierSemaphore = CreateSemaphore(NULL, 0, MAX_NUMBER_OF_GUESTS, "barrierSemaphore");
-	FILE *fp = NULL;
-
+	FILE *rooms_fp = NULL;
+	FILE *names_fp = NULL;
+	guest *guests_array[MAX_NUMBER_OF_GUESTS];
 	room *rooms_array[MAX_ROOMS];
 	int room_index = 0;
+	int names_index = 0;
 	//char path[] = argv[1] + rooms.txt
-	char *path;
+	char *rooms_path;
+	char *names_path;
+	char line[MAX_LINE_LEN] = "";
 
-	int len = strlen(argv[1]) + ROOMS_FILE_NAME;
-	path = (char *)malloc(len);
-	if (path == NULL) {
+	//create rooms file path
+	int rooms_names_len = strlen(argv[1]) + ROOMS_NAMES_FILE_NAME;
+	rooms_path = (char *)malloc(rooms_names_len);
+	if (rooms_path == NULL) {
 		printf("Memory allocation error");
 		/* error handle*/
 		exit(1);
 	}
-	strcpy_s(path, len, argv[1]);
-	strcat_s(path, len, "\\rooms.txt");
+	strcpy_s(rooms_path, rooms_names_len, argv[1]);
+	strcat_s(rooms_path, rooms_names_len, "\\rooms.txt");
 
+	//create names file path
+	names_path = (char *)malloc(rooms_names_len);
+	if (names_path == NULL) {
+		printf("Memory allocation error");
+		/* error handle*/
+		exit(1);
+	}
+	strcpy_s(names_path, rooms_names_len, argv[1]);
+	strcat_s(names_path, rooms_names_len, "\\names.txt");
+
+	//Get rooms from file
 	int file_error;
-	file_error = fopen_s(&fp, path, "r");
+	file_error = fopen_s(&fp, rooms_path, "r");
 	if (file_error) {
-		printf("error opening file in path %s", path);
+		printf("error opening file in path %s", rooms_path);
 	}
 
-	while (feof(fp) == 0) {
-		char line[MAX_LINE_LEN] = "";
-		fgets(line, MAX_LINE_LEN, fp);
+	while (feof(rooms_fp) == 0) {
+		///////////////reset line
+		fgets(line, MAX_LINE_LEN, rooms_fp);
 		RemoveNewLine(&line);
 		CreateRoom_UpdateArray(line, rooms_array, room_index);
 		/* check that the array is updated with the correct room */
 		printf("%s %d %d\n", rooms_array[room_index]->name, rooms_array[room_index]->price_for_night, rooms_array[room_index]->max_guests);
 		room_index++;	
 	}
-	getchar();
+	getchar();///???????
+
+	//Get names from file
+	file_error = fopen_s(&fp, names_path, "r");
+	if (file_error) {
+		printf("error opening file in path %s", names_path);
+	}
+
+	while (feof(names_fp) == 0) {
+		/////reset line
+		fgets(line, MAX_LINE_LEN, names_fp);
+		RemoveNewLine(&line);
+		CreateGuests_UpdateArray(line, names_array, names_index);
+		names_index++;
+	}
+
+
+	
 }
 
 void CreateRoom_UpdateArray(char str[], room *room_array[], int index) {
@@ -85,10 +118,23 @@ void CreateRoom_UpdateArray(char str[], room *room_array[], int index) {
 	sscanf_s(guests_str, "%d", &max_guests);
 
 
-	UpdateArray(room_array, room_name, max_guests, price_for_night, index);
+	UpdateArrayRooms(room_array, room_name, max_guests, price_for_night, index);
 	
 }
+
+void CreateGuestsUpdateArray(char str[], guest* names_array[], int index) {
+	char delim[2] = " ";
+	char* name;
+	char* nigths;
+	int nights_i;
+
+	name = strtok(str, delim);
+	nights = strtok(str, delim);
+	nights_i = atoi(nights);
 	
+	UpdateArrayNames(names_array, name, nights_i, index);
+
+}
 void RemoveNewLine(char* str) {
 	int len = strlen(str);
 	if (str[len - 1] == '\n') {
@@ -97,7 +143,7 @@ void RemoveNewLine(char* str) {
 	
 }
 
-void UpdateArray(room *room_array[], char room_name[], int max_guests, int price, int index) {
+void UpdateArrayRooms(room *room_array[], char room_name[], int max_guests, int price, int index) {
 	room *r_ptr = (room *)malloc(sizeof(room));
 	if (NULL == r_ptr) {
 		printf("Memory allocation error");
@@ -108,4 +154,16 @@ void UpdateArray(room *room_array[], char room_name[], int max_guests, int price
 	r_ptr->price_for_night = price;
 	strcpy_s(r_ptr->name, ROOM_GUEST_NAME_LEN, room_name);
 	room_array[index] = r_ptr;
+}
+
+void UpdateArrayNames(guest *names_array[], char name[], int nights, int index) {
+	guest *n_ptr = (room *)malloc(sizeof(guest));
+	if (NULL == n_ptr) {
+		printf("Memory allocation error");
+		/* error handle*/
+		exit;
+	}
+	n_ptr->nigths = nights;
+	strcpy_s(n_ptr->name, ROOM_GUEST_NAME_LEN, name);
+	names_array[index] = n_ptr;
 }
